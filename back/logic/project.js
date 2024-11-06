2c26c4be30a25b2e35f6da540d19688b76a1c2d0
munhwas1140 <munhwas1140@gmail.com>
2024-11-06 18:48:14 +0900

    indent

--- back/logic/project.js
+++ back/logic/project.js
@@ -4,82 +4,82 @@ const Mutex = require('async-mutex').Mutex;
 
 
 const projectDeleteErr = {
-	errors: 400,
-	message: "Cannot delete. because tasks are associated with this project."
+    errors: 400,
+    message: "Cannot delete. because tasks are associated with this project."
 };
 
 const mutex = new Mutex();
 
 // POST
 async function postProject(req, res) {
-	try {
-		await mutex.acquire();
-		let { projectId, projects } = await repository.getProjects();
+    try {
+        await mutex.acquire();
+        let { projectId, projects } = await repository.getProjects();
 
-		projectId += 1;
-		const newProject = {
-			id: projectId,
-			title: req.body.title,
-			description: req.body.description,
-			tasks: []
-		};
-		projects.push(newProject);
+        projectId += 1;
+        const newProject = {
+            id: projectId,
+            title: req.body.title,
+            description: req.body.description,
+            tasks: []
+        };
+        projects.push(newProject);
 
-		await repository.saveProjects({ projectId, projects })
-		res.status(201).json({
-			id: newProject.id, 
-			title: newProject.title,
-		});
-	} finally {
-		mutex.release();
-	}
+        await repository.saveProjects({ projectId, projects })
+        res.status(201).json({
+            id: newProject.id, 
+            title: newProject.title,
+        });
+    } finally {
+        mutex.release();
+    }
 
 }
 
 // GET ALL
 async function getProjects(req, res) {
-	const { projects } = await repository.getProjects();
-	res.status(200).json(projects);
+    const { projects } = await repository.getProjects();
+    res.status(200).json(projects);
 }
 
 // GET DETAIL
 async function getProjectDetail(req, res) {
-	const { projects } = await repository.getProjects();
-	const { project } = findProjectById(projects, req.params.projectId, res);
-	if(!project) {
-		return ;
-	}
-	
-	let { tasks } = await repository.getTasks();
-	const projectWithTasks = {
-		...project,
-		tasks: tasks.filter(task => task.pjId == project.id)
-	};
-	res.status(200).json(projectWithTasks);
+    const { projects } = await repository.getProjects();
+    const { project } = findProjectById(projects, req.params.projectId, res);
+    if(!project) {
+        return ;
+    }
+    
+    let { tasks } = await repository.getTasks();
+    const projectWithTasks = {
+        ...project,
+        tasks: tasks.filter(task => task.pjId == project.id)
+    };
+    res.status(200).json(projectWithTasks);
 }
 
 // DELETE
 async function deleteProject(req, res) {
-	try {
-		await mutex.acquire();
-		let { projectId, projects } = await repository.getProjects();
-		const { projectIdx } = findProjectById(projects, req.params.projectId, res);
-		if(projectIdx == -1) {
-			return ;
-		}
+    try {
+        await mutex.acquire();
+        let { projectId, projects } = await repository.getProjects();
+        const { projectIdx } = findProjectById(projects, req.params.projectId, res);
+        if(projectIdx == -1) {
+            return ;
+        }
 
-		const project = projects[projectIdx];
-		if(project.tasks.length > 0) {
-			res.status(400).json(projectDeleteErr);
-			return ;
-		}
+        const project = projects[projectIdx];
+        if(project.tasks.length > 0) {
+            res.status(400).json(projectDeleteErr);
+            return ;
+        }
 
-		projects.splice(projectIdx, 1);
-		await repository.saveProjects({ projectId, projects })
-		res.status(200).json({message: "project successfully deleted"});
-	} finally {
-		mutex.release();
-	}
+        projects.splice(projectIdx, 1);
+        await repository.saveProjects({ projectId, projects })
+        res.status(200).json({message: "project successfully deleted"});
+    } finally {
+        mutex.release();
+    }
 }
 
 // util functions
@@ -100,10 +100,10 @@ function findProjectById(projects, projectId, res) {
 }
 
 module.exports = {
-	postProject,
-	getProjects,
-	getProjectDetail,
-	deleteProject,
-	findProjectById,
+    postProject,
+    getProjects,
+    getProjectDetail,
+    deleteProject,
+    findProjectById,
 };
 
